f33b7f0519b117c888eea0fa4d52c812805735cf
Anime-Streaming <82533200+Anime-Streaming@users.noreply.github.com>
2021-04-16 16:09:24 +0200

    Add files via upload

---
+++ js/Lpasdsfini.js
@@ -0,0 +1,65 @@
+//alert("Attention cette page est en cours de développement.");
+
+/*<img src="DR stone.png"
+     alt="Dr Stone est un anime qui traite d'un futur ou le monde serait transfomer en pierre."
+     width="400"
+     height="400"
+     title="Dr stone un bon anime?"></img>*/
+
+/*do{
+alert('Veuillez entrer un prénom valide'); prompt('Quel est ton prénom?');
+console.log("L'utilisateur a rentré un prénom incrorrect");	
+}
+while(prénom = "")*/
+
+/*var prénom = prompt("Quel est ton prénom?");
+console.log("L'utilisateur a rentré son prénom");
+
+if (prénom == "") {
+  alert("Veuillez entrer un prénom valide");
+  prompt("Quel est ton prénom?");
+  console.log("L'utilisateur a rentré un prénom incrorrect");
+} else if (typeof prénom == "undefined") {
+  alert("Veuillez entrer un prénom valide");
+  prompt("Quel est ton prénom?");
+  console.log("L'utilisateur a rentré un prénom incrorrect");
+}
+var nom = prompt("Quel est ton nom de famille?");
+console.log("L'utilisateur a rentré son nom");
+
+if (nom == "") {
+  alert("Veuillez entrer un nom valide");
+  prompt("Quel est ton nom?");
+  console.log("L'utilisateur a rentré un nom incrorrect");
+}
+
+var pseudo = prénom + " " + nom;
+
+/*if (typeof pseudo != "undefined" || pseudo != "") {
+  alert("Votre pseudo n'a pas pu être enregistré.");
+  console.log("Le pseudo de l'utilisateur n'a pas pu être enregistré.");*/
+// alert("Vous pouvez tout de même continuer sur le site sans problèmes.");
+
+/*console.log("Validation du pseudo");
+var Validation = prompt("Veux tu que ton pseudo soit " + pseudo);
+
+var Validation2 = "oui || Oui";
+var Validation3 = "non || Non";
+
+if (Validation == Validation2) {
+  console.log("L'utilisateur obtient son pseudo");
+  var pseudo = "Ton  pseudo sera donc " + prénom + " " + nom;
+  alert(pseudo);
+  alert("Vous pouvez continuer sur le site sans problèmes.");
+} else if (Validation == Validation3) {
+  console.log("L'utilisateur veut choisir un autre pseudo.");
+  var pseudo = prompt("Quel pseudo souhaite tu avoir?");
+} else if (typeof pseudo != Validation3 || Validation3) {
+  alert("Veuillez entrer un pseudo valide");
+  prompt("Quel est ton pseudo?");
+  console.log("L'utilisateur a rentré un prénom incrorrect");
+} else {
+  alert("Veuillez entrer une réponse correct (oui/non).");
+  prompt("Veux tu que ton pseudo soit" + pseudo);*/
+
+// à résoudre (Système inscription+ affichage du pseudo/Système replissage pseudo + affichage)
